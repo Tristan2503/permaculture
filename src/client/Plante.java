c798114ca46db6f0e0a31b75770893afae3425e8
mattdelcroix <matt.delcroix@gmail.com>
2020-12-18 10:27:07 +0100

    Classes, setter/getter, constructeur)

--- src/client/Plante.java
+++ src/client/Plante.java
@@ -15,14 +15,80 @@ public class Plante {
 	
 	public Plante(String nom, String description, int duree_pousse, Date date_debut_semis, Date date_fin_semis, 
 			niveauIrrigation arrosage, int taille, couleurPlante couleur) {
-		this.nom = nom;
-		this.description = description;
-		this.duree_pousse = duree_pousse;
-		this.date_debut_semis = date_debut_semis;
-		this.date_fin_semis = date_fin_semis;
-		this.arrosage = arrosage;
-		this.taille = taille;
-		this.couleur = couleur;		
+		this.setNom(nom);
+		this.setDescription(description);
+		this.setDuree_pousse(duree_pousse);
+		this.setDate_debut_semis(date_debut_semis);
+		this.setDate_fin_semis(date_fin_semis);
+		this.setArrosage(arrosage);
+		this.setTaille(taille);
+		this.setCouleur(couleur);		
 	}
+	
+	//getters + setters
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getDuree_pousse() {
+		return duree_pousse;
+	}
+
+	public void setDuree_pousse(int duree_pousse) {
+		this.duree_pousse = duree_pousse;
+	}
+
+	public Date getDate_debut_semis() {
+		return date_debut_semis;
+	}
+
+	public void setDate_debut_semis(Date date_debut_semis) {
+		this.date_debut_semis = date_debut_semis;
+	}
+
+	public Date getDate_fin_semis() {
+		return date_fin_semis;
+	}
+
+	public void setDate_fin_semis(Date date_fin_semis) {
+		this.date_fin_semis = date_fin_semis;
+	}
+
+	public niveauIrrigation getArrosage() {
+		return arrosage;
+	}
+
+	public void setArrosage(niveauIrrigation arrosage) {
+		this.arrosage = arrosage;
+	}
+
+	public int getTaille() {
+		return taille;
+	}
+
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+
+	public couleurPlante getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(couleurPlante couleur) {
+		this.couleur = couleur;
+	}
+
 
 }
--- src/client/Terrain.java
+++ src/client/Terrain.java
@@ -3,9 +3,42 @@ package client;
 import java.util.List;
 
 public class Terrain {
-	public String nom;
-	public int longueur;
-	public int largeur;
-	public List<Plante> plantes;
-
+	private String nom;
+	private int longueur;
+	private int largeur;
+	private List<Plante> plantes;
+	
+	public Terrain(String nom, int longueur, int largeur, List<Plante> plantes) {
+		this.setNom(nom);
+		this.setLongueur(longueur);
+		this.setLargeur(largeur);
+		this.setPlantes(plantes);
+	}
+	
+	
+	//Getter et Setter
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getLongueur() {
+		return longueur;
+	}
+	public void setLongueur(int longueur) {
+		this.longueur = longueur;
+	}
+	public int getLargeur() {
+		return largeur;
+	}
+	public void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+	public List<Plante> getPlantes() {
+		return plantes;
+	}
+	public void setPlantes(List<Plante> plantes) {
+		this.plantes = plantes;
+	}
 }
